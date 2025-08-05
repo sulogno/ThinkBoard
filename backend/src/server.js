2a0ea7f0ad78ba7c12d27ebb837e7d5ccb32c530
Sulogno Sarkar <rhitsarkar2003@gmail.com>
2025-08-05 12:39:51 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,10 @@ import dotenv from "dotenv";
 import notesroutes from "./routes/notesroutes.js";
 import { connectdb } from "./config/db.js";
 import rateLimit from "express-rate-limit";
+import path from "path"
 dotenv.config();
+
+const __dirname = path.resolve();
 
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000,
@@ -18,9 +21,19 @@ const limiter = rateLimit({
 });
 
 const app = express();
+
+if(process.env.NODE_ENV !== "production"){
 app.use(cors({ origin: "http://localhost:5173" }));
+}
 app.use(express.json());
 app.use("/api/notes", limiter, notesroutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+  app.get("/{*splat}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectdb();
 
