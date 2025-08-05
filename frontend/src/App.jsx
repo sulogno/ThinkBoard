7c76ffef4366c755a8f8cefdd0cdb88203ef9d6c
Sulogno Sarkar <rhitsarkar2003@gmail.com>
2025-08-05 12:54:55 +0530

    ready for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import HomePage from "./pages/HomePaage";
-import Createpage from "./pages/createPage";
+import Createpage from "./pages/CreatePage";
 import NoteDetailPage from "./pages/NoteDetailPage";
 import { Route, Routes } from "react-router-dom";
 import toast from "react-hot-toast";
